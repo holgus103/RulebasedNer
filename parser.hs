7d170b8b0517d23c2f024c6d5fb03744f9ece4e0
holgus103 <holgus103@gmail.com>
2018-04-26 02:17:06 +0200

    Data parser

---
+++ parser.hs
@@ -0,0 +1,35 @@
+{-# LANGUAGE DuplicateRecordFields, DeriveDataTypeable #-}
+module Parser where
+
+import Text.JSON
+import Text.JSON.Generic
+import System.IO
+
+loadDataset :: String -> IO [Entry]
+loadDataset path = do
+    input <- readFile path
+    return ((decodeJSON input)::[Entry]) 
+
+
+
+
+data Subentity = Subentity {
+    end :: Int,
+    value :: String,
+    entity :: String,
+    start :: Int
+} deriving (Eq, Show, Data, Typeable)
+
+data MainEntity = MainEntity {
+    end :: Int,
+    value :: String,
+    entity :: String,
+    start :: Int,
+    subentities :: [Subentity]
+} deriving (Eq, Show, Data, Typeable)
+    
+data Entry = Entry {
+    text :: String,
+    entities :: [MainEntity]
+    
+} deriving (Eq, Show, Data, Typeable)
